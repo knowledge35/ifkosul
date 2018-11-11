34bb3beb3049471bd8aeedf9e9a2a0acf701e88b
scientiest35 <ibomyakmc@hotmasdail.com>
2018-11-11 07:22:51 +0300

    update

---
+++ ifkosul/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+
+
+int main() {
+
+	int i;
+	scanf("%d",&i);
+	if (i>5)
+	{
+		printf("\nbüyük");
+
+	}
+	else
+	{
+		printf("\nbüyük degil");
+
+	}
+	printf("\nbitti");
+	_getch();
+	return 0;
+}
